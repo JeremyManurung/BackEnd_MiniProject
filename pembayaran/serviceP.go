7a301a206e58d79ebde50a18cbfddc4478ecc194
Jeremy Sisto Manurung <87016966+JeremyManurung@users.noreply.github.com>
2021-12-03 16:13:06 +0700

    Update serviceP.go

--- pembayaran/serviceP.go
+++ pembayaran/serviceP.go
@@ -1,49 +1,49 @@
-package pembayaran
+// package pembayaran
 
-import (
-	"strconv"
-	"minipro/user"
-	midtrans "github.com/veritrans/go-midtrans"
-)
+// import (
+// 	"strconv"
+// 	"minipro/user"
+// 	midtrans "github.com/veritrans/go-midtrans"
+// )
 
-type service struct{
+// type service struct{
 
-}
+// }
 
-type Service interface{
-	GetPembayaranUrl(transaksi Transaksi, user user.User) (string, error)
-}
+// type Service interface{
+// 	GetPembayaranUrl(transaksi Transaksi, user user.User) (string, error)
+// }
 
-func NewService() *service{
-	return &service{}
-}
+// func NewService() *service{
+// 	return &service{}
+// }
 
-func (s *service) GetPembayaranUrl(transaksi Transaksi, user user.User) (string, error){
-	midclient := midtrans.NewClient()
-    midclient.ServerKey = ""
-    midclient.ClientKey = ""
-    midclient.APIEnvType = midtrans.Sandbox
+// func (s *service) GetPembayaranUrl(transaksi Transaksi, user user.User) (string, error){
+// 	midclient := midtrans.NewClient()
+//     midclient.ServerKey = ""
+//     midclient.ClientKey = ""
+//     midclient.APIEnvType = midtrans.Sandbox
 
     
-    snapGateway := midtrans.SnapGateway{
-        Client: midclient,
-    }
+//     snapGateway := midtrans.SnapGateway{
+//         Client: midclient,
+//     }
 
-	snapReq := &midtrans.SnapReq{
-		CustomerDetail: &midtrans.CustDetail{
-			Email: user.Email,
-			FName : user.Nama,
-		},
-		TransactionDetails: midtrans.TransactionDetails{
-            OrderID: strconv.Itoa(transaksi.ID),
-            GrossAmt: int64(transaksi.JumlahUang),
-        },
-	}
+// 	snapReq := &midtrans.SnapReq{
+// 		CustomerDetail: &midtrans.CustDetail{
+// 			Email: user.Email,
+// 			FName : user.Nama,
+// 		},
+// 		TransactionDetails: midtrans.TransactionDetails{
+//             OrderID: strconv.Itoa(transaksi.ID),
+//             GrossAmt: int64(transaksi.JumlahUang),
+//         },
+// 	}
 
-	snapTokenResp, err:= snapGateway.GetToken(snapReq)
-	if err != nil{
-		return "", err
-	}
+// 	snapTokenResp, err:= snapGateway.GetToken(snapReq)
+// 	if err != nil{
+// 		return "", err
+// 	}
 
-	return snapTokenResp.RedirectURL, nil
-}
+// 	return snapTokenResp.RedirectURL, nil
+// }
